e3df5a31785a08c363df7833e4e81a5c8e77e908
Mirco Dotta <mirco.dotta@gmail.com>
2018-06-28 10:31:31 +0200

    Follow Scala convention for placing package objects

---
+++ framework/src/play/src/main/scala/views/html/package.scala
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package views
+
+/**
+ * Contains ready to use built-in templates and template helpers for html templates.
+ */
+package object html
--- framework/src/play/src/main/scala/views/txt/package.scala
+++ framework/src/play/src/main/scala/views/txt/package.scala
@@ -2,6 +2,6 @@
  * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
  */
 
-package play
+package views
 
-package object api
+package object txt
